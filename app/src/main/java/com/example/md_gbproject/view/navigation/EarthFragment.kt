b54370cb8791da234264556d8971dd610994525c
PolinaRrr <poniapocalypsisa@gmail.com>
2022-06-09 23:08:45 +0300

    HW 3
    Add 3 fragments

---
+++ app/src/main/java/com/example/md_gbproject/view/navigation/EarthFragment.kt
@@ -0,0 +1,74 @@
+package com.example.md_gbproject.view.navigation
+
+import android.content.Context
+import android.os.Bundle
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.md_gbproject.R
+import com.example.md_gbproject.databinding.FragmentEarthBinding
+import com.example.md_gbproject.utils.CHOSEN_THEME
+import com.example.md_gbproject.utils.LOCAL_SP
+
+class EarthFragment : Fragment() {
+
+    private var _binding: FragmentEarthBinding? = null
+    private val binding: FragmentEarthBinding
+        get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val localContext: Context = ContextThemeWrapper(activity, getIdTheme(getCurrentIdTheme()))
+        val localInflater = inflater.cloneInContext(localContext)
+        _binding = FragmentEarthBinding.inflate(localInflater,container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_bottom_bar, menu)
+    }
+
+
+
+
+    private fun getIdTheme(idTheme: Int): Int {
+        return idTheme
+    }
+
+    private fun getCurrentIdTheme(): Int {
+        val sharedPreferences =
+            requireActivity().getSharedPreferences(LOCAL_SP, AppCompatActivity.MODE_PRIVATE)
+        return sharedPreferences.getInt(CHOSEN_THEME, -1)
+    }
+
+    private fun setCurrentTheme(idTheme: Int) {
+        val sharedPreferences =
+            requireActivity().getSharedPreferences(LOCAL_SP, AppCompatActivity.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putInt(CHOSEN_THEME,idTheme)
+        editor.apply()
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = EarthFragment()
+    }
+}
