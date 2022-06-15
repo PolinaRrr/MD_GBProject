688e14e17379e35ee6e59356ae44d7d3fb2921b2
PolinaRrr <poniapocalypsisa@gmail.com>
2022-06-15 22:58:17 +0300

    HW 3
    +icons

--- app/src/main/java/com/example/md_gbproject/view/ApiActivity.kt
+++ app/src/main/java/com/example/md_gbproject/view/ApiActivity.kt
@@ -2,6 +2,7 @@ package com.example.md_gbproject.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.md_gbproject.R
 import com.example.md_gbproject.databinding.ActivityApiBinding
 import com.example.md_gbproject.view.navigation.viewpager.ViewPagerAdapter
 
@@ -13,5 +14,12 @@ class ApiActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.viewPager.adapter = ViewPagerAdapter(supportFragmentManager)
         binding.viewPagerTabLayout.setupWithViewPager(binding.viewPager)
+
+        binding.viewPagerTabLayout.getTabAt(0)?.setIcon(R.drawable.ic_planet_earth_outline)
+        binding.viewPagerTabLayout.getTabAt(1)?.setIcon(R.drawable.ic_solar_system_outline)
+        binding.viewPagerTabLayout.getTabAt(2)?.setIcon(R.drawable.ic_planet_mars_outline)
+
+
+
     }
 }
