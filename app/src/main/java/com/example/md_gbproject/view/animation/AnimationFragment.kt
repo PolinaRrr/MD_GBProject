efbc3faf6c964ee781f002a3237cf6f5c015a34f
PolinaRrr <poniapocalypsisa@gmail.com>
2022-06-27 12:40:23 +0300

    HW 5
    transition mode

--- app/src/main/java/com/example/md_gbproject/view/animation/AnimationFragment.kt
+++ app/src/main/java/com/example/md_gbproject/view/animation/AnimationFragment.kt
@@ -5,6 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.transition.ChangeBounds
+import androidx.transition.Fade
+import androidx.transition.TransitionManager
+import androidx.transition.TransitionSet
 import com.example.md_gbproject.databinding.FragmentAnimationBinding
 
 class AnimationFragment : Fragment() {
@@ -25,20 +29,24 @@ class AnimationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         binding.buttonAnimate01.setOnClickListener {
+            changeTransition()
+
             if (binding.buttonAnimate02.visibility == View.INVISIBLE) {
                 binding.buttonAnimate02.visibility = View.VISIBLE
                 binding.buttonAnimate01.visibility = View.INVISIBLE
-            }else{
+            } else {
                 binding.buttonAnimate01.visibility = View.VISIBLE
             }
         }
         binding.buttonAnimate02.setOnClickListener {
+            changeTransition()
             if (binding.buttonAnimate01.visibility == View.INVISIBLE) {
                 binding.buttonAnimate01.visibility = View.VISIBLE
                 binding.buttonAnimate02.visibility = View.INVISIBLE
 
-            }else{
+            } else {
                 binding.buttonAnimate02.visibility = View.VISIBLE
             }
         }
@@ -54,6 +62,17 @@ class AnimationFragment : Fragment() {
     companion object {
         @JvmStatic
         fun newInstance() = AnimationFragment()
+    }
+
+    private fun changeTransition(){
+        val transitionBounds = ChangeBounds()
+        transitionBounds.duration = 5000
+        val transitionFade = Fade()
+        transitionFade.duration = 5000
+        val transitionSet = TransitionSet()
+        transitionSet.addTransition(transitionFade)
+        transitionSet.addTransition(transitionBounds)
+        TransitionManager.beginDelayedTransition(binding.root,transitionSet)
     }
 
 
