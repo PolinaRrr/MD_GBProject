82da128a455ef2f4edf352791f0f4929e1a258ae
PolinaRrr <poniapocalypsisa@gmail.com>
2022-06-27 18:49:14 +0300

    HW 5
    smth wrong

--- app/src/main/java/com/example/md_gbproject/view/animation/PhotoTransitionFragment.kt
+++ app/src/main/java/com/example/md_gbproject/view/animation/PhotoTransitionFragment.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.transition.ChangeBounds
+import androidx.transition.ChangeImageTransform
+import androidx.transition.TransitionManager
+import androidx.transition.TransitionSet
 import com.example.md_gbproject.databinding.FragmentPhotoTransationBinding
 import com.example.md_gbproject.viewmodel.AppViewModel
 
@@ -15,6 +21,7 @@ class PhotoTransitionFragment : Fragment() {
     private val binding: FragmentPhotoTransationBinding
         get() = _binding!!
 
+    private var isOpen: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,6 +38,36 @@ class PhotoTransitionFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.photoTransition.setOnClickListener {
+            isOpen = !isOpen
+            val transitionBounds = ChangeBounds()
+            transitionBounds.duration = 5000
+
+            val transitionImage = ChangeImageTransform()
+            transitionImage.duration = 5000
+
+            val transitionSet = TransitionSet()
+            transitionSet.addTransition(transitionImage)
+            transitionSet.addTransition(transitionBounds)
+
+            TransitionManager.beginDelayedTransition(binding.root, transitionSet)
+
+            binding.photoTransition.scaleType = if (isOpen) {
+                ImageView.ScaleType.CENTER_CROP
+            } else {
+                ImageView.ScaleType.CENTER_INSIDE
+            }
+            val params = (binding.photoTransition.layoutParams as ConstraintLayout.LayoutParams)
+            params.height = if (isOpen) {
+                ConstraintLayout.LayoutParams.MATCH_PARENT
+            } else {
+                ConstraintLayout.LayoutParams.WRAP_CONTENT
+            }
+
+
+            binding.photoTransition.layoutParams = params
+        }
 
     }
 
